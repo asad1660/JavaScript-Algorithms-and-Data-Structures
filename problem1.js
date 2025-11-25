603fb58db3607a52445aec6482d5c74d8bc6cb22
AsadKhan1660 <Asad.Khan@ihorizons.com>
2025-11-26 02:34:14 +0500

    refactoring

--- problem1.js
+++ problem1.js
@@ -67,3 +67,14 @@ console.log(charCount("Hi there!"));
 //0-9 = 48-57
 //a-z = 97-122
 //A-Z = 65-90
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha (A-Z)
+    !(code > 96 && code < 123) // lower alpha (a-z)
+  ) {
+    return false;
+  }
+  return true;
+}
