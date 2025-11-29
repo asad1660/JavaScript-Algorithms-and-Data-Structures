025082a1cfa4f4648d8982ac41abe43deeed6781
AsadKhan1660 <Asad.Khan@ihorizons.com>
2025-11-29 19:57:16 +0500

    perfomance issues resolved

--- problem2.js
+++ problem2.js
@@ -30,17 +30,43 @@
 // the issue with the above solution is the edge case where same frequency is not maintained
 //eg: input([1,2,3,2]) output([1,4,9,1]) //false (must be same frequency)
 
+// function same(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2);
+//     if (correctIndex == -1) {
+//       return false;
+//     }
+//     arr2.splice(correctIndex, 1);
+//   }
+//   return true;
+// }
+// console.log(same([1, 2, 3, 2], [1, 4, 9, 4]));
+//the issue with the above solution is that it has a time complexity of O(n^2)
+//because of the nested loop , if 1000items in a1 it will have to run 1000*1000 times in worst case
+
 function same(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
+  const freq1 = {};
+  const freq2 = {};
+  for (let item of arr1) {
+    freq1[item] = (freq1[item] || 0) + 1;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2);
-    if (correctIndex == -1) {
+  for (let item2 of arr2) {
+    freq2[item2] = (freq2[item2] || 0) + 1;
+  }
+
+  for (let key in freq1) {
+    if (!(key ** 2 in freq2)) {
       return false;
     }
-    arr2.splice(correctIndex, 1);
+
+    if (!(freq2[key ** 2] == freq1[key])) {
+      return false;
+    }
   }
   return true;
 }
+
 console.log(same([1, 2, 3, 2], [1, 4, 9, 4]));
