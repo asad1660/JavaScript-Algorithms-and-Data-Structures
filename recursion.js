1884dc5e1b73f53bc4e92562ccd5cb6f6dd87f76
AsadKhan1660 <Asad.Khan@ihorizons.com>
2025-12-22 14:41:53 +0000

    Recursion problem solving

--- recursion.js
+++ recursion.js
@@ -17,3 +17,14 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 console.log(factorial(3));
+
+/* 
+This Example is about Array of a number multiply with the previous one in javascript.
+how we can achieve this by doing recursion
+*/
+function productOfArray(array) {
+  if (array.length === 0) return 1;
+  return array[0] * productOfArray(array.slice(1));
+}
+
+console.log(productOfArray([1, 2, 3, 10]));
